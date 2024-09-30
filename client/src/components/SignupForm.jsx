619939bd1ef187efbf7e4c850dae8c2dd9ad8d5e
aharper2568 <anthonyharper568@gmail.com>
2024-09-30 17:51:31 -0400

    partially refactored sign up form to use mutations isntead of old API

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { ADD_USER } from '../utils/mutations';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client'
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [addUser, { error }] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -28,7 +30,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser({
+        variables: {
+          username: userFormData.username,
+          email: userFormData.email,
+          password: userFormData.password
+        }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
